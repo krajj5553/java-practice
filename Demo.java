aa1a2b5849a2d0be8fd3be15716420937cb8201c
krajj5553 <krajj5553@gmail.com>
2022-01-08 00:42:42 +0530

    Print Statement Practice

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args)
+    {
+        System.out.println("tab practice.\tThis is a tab");
+        System.out.println("Next Line practice.\nThis is a New Line");
+    }
+    
+}
